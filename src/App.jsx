2699661f9c79cfc0fa6052aaff38c319009ad80e
PPairan33 <cyborgrazer@googlemail.com>
2026-05-12 13:11:13 +0200

    feat: add animated border button and button component; implement footer and navbar layouts; create sections for about, experience, hero, and projects; integrate Tailwind CSS for styling; set up Vite configuration

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+
+import { Footer } from "./layout/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
